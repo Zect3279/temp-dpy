b752024136c4b28b2cb1cb2cbfdd7051859d2a10
zect <s.a.s.jishu@gmail.com>
2021-03-31 08:17:06 +0900

    [Add]: 完成（多分）

---
+++ cogs/admin.py
@@ -0,0 +1,28 @@
+import discord
+from discord.ext import commands
+
+
+
+
+class Admin(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+
+    @commands.command()
+    async def hello(self,ctx):
+        await ctx.send("hello")
+
+
+
+
+
+
+
+
+
+
+
+
+def setup(bot):
+    bot.add_cog(Admin(bot))
